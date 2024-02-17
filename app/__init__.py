220c0bc39ebdf90bbaa927ed2b98479f341e7082
James <james@jamesmcc.co.uk>
2024-02-17 19:09:51 +0000

    Added Docker Stuff

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,21 @@ import os
 db = SQLAlchemy()
 migrate = Migrate()
 
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_ADDRESS = os.getenv('DB_ADDRESS')
+DB_PORT = os.getenv('DB_PORT')
+DB_DATABASE = os.getenv('DB_DATABASE')
+
+DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}" \
+        f"@{DB_ADDRESS}:{DB_PORT}/{DB_DATABASE}"
+
+
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = \
-        f"mysql+pymysql://root:thisisasupersecurepassword" \
-        f"@sussex.thejmc.net:25560/db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
     db.init_app(app)
