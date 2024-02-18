5cac10c3ca2cb25cda9c18d84963be3a791c842b
James <james@jamesmcc.co.uk>
2024-02-18 11:32:27 +0000

    Added SFX

--- app/static/js/plant_view.js
+++ app/static/js/plant_view.js
@@ -12,6 +12,33 @@ function mute_toggle() {
         button.dataset.muted = "True"
         button.children.item(0).className = "bi bi-volume-mute"
     }
+    play_sound()
+}
+
+function play_sound() {
+    let button = document.getElementById("mute-button")
+    let current_state = button.dataset.muted;
+    let water_state = document.getElementById("water_val").value
+    let sunlight_state = document.getElementById("sunlight_val").value
+    console.log(parseInt(water_state))
+    console.log(parseInt(sunlight_state))
+
+    // if (current_state === "False") {
+    if (parseInt(water_state)  > 80) {
+        console.log("water")
+        let under_the_water = document.getElementById("drowning")
+        under_the_water.play()
+    }
+    if (parseInt(sunlight_state) < 20) {
+        console.log("light")
+        document.getElementById("scream").play()
+    }
+    if (parseInt(sunlight_state) >= 20 && parseInt(water_state) < 80) {
+        console.log("all good")
+        document.getElementById("just_right").play()
+
+    }
+    // }
 }
 
 
